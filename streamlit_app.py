7ab1819e062cff7c07725b224f7e6118e251d491
OmarAboz <105570953+OmarAboz@users.noreply.github.com>
2023-06-02 11:08:14 +0400

    Update streamlit_app.py - Added multiselect and indexed the data frame
    Indexed the data frame to Fruit column and added multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,4 +14,10 @@ sl.text("🥑🍞 Avocado Toast")
 # Creating Smoothie section
 sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list_index = my_fruit_list.set_index('Fruit')
+
+# Allow selection of the fruits
+sl.multiselect("Please pick some fruits: ", list(my_fruit_list_index))
+
+#Display the data frame for the user to select the items
 sl.dataframe(my_fruit_list)
