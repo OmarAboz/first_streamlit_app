50c45e9c4dc89d04f5ef1e2e9ce71000eb0a0558
OmarAboz <105570953+OmarAboz@users.noreply.github.com>
2023-06-02 10:55:10 +0400

    Update streamlit_app.py - Added icons and new header
    added new header for the app along with new icons

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,12 @@ import streamlit as sl
 sl.title("My Parents Healthy Diner")
 
 sl.header("Breakfast Menu")
-sl.text("Omega 3 & Blueberry Oatmeal")
-sl.text("Kale, Spinach & Rocket Smoothie")
-sl.text("Hard-Boiled Free-Range Egg")
+
+# Main healthy options
+sl.text("🥣 Omega 3 & Blueberry Oatmeal")
+sl.text("🥗 Kale, Spinach & Rocket Smoothie")
+sl.text("🐔 Hard-Boiled Free-Range Egg")
+sl.text("🥑🍞 Avocado Toast")
+
+# Creating Smoothie section
+sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
